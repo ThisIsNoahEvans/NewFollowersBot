41221035f940808d356dd287833bea86870d4587
Noah Evans <noah@itsnoahevans.co.uk>
2020-09-01 16:22:29 +0100

    Finished remove-users.py
    The bot is pretty much finished now, unless I have a 3am thought of something that's broken.

--- remove-users.py
+++ remove-users.py
@@ -14,6 +14,30 @@ def removeUsers():
 
     # Open the users file
     usersFile = open('users.txt', 'w')
+    usersFileLines = usersFile.readlines
 
-    for follower in botFollowers:
-        # For each follower in the bot followers
+    for follower in usersFileLines:
+        # For each follower that's saved
+        if follower in botFollowers:
+            # If they currently follow the bot
+            # Do nothing
+            pass
+        else:
+            # If they do not follow the bot (they have unfollowed)
+            # Get the data from the current version (local to this program) of the file
+            fileData = usersFile.read()
+            # Data to remove
+            data = follower + '\n'
+            # Remove the data (replace it with nothing)
+            fileData.replace(data, '')
+            # Clear the file
+            usersFile.seek(0)
+            usersFile.truncate(0)
+            # Write the new data
+            usersFile.write(fileData)
+            # Close the file
+            usersFile.close()
+            
+            
+            
+            
